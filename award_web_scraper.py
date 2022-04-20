299c58e13c2b616ae0d62e5adb981c8f369e5498
austin-py <81866885+austin-py@users.noreply.github.com>
2022-04-20 10:27:05 -0500

    Created file for web_scraper

---
+++ award_web_scraper.py
@@ -0,0 +1,7 @@
+
+# Basic interface: 
+
+ceremony_name = input('What is the name of the award-show you would like information about')
+ceremony_year = input("What year would yuu like information about {ceremony_name} for?")
+
+print("The ceremony we are gathering data for is {ceremony_name} in {ceremony_year}")

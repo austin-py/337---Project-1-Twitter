ecbfe644f4c43dbee3accd98ee4db0964c4d1b4c
lfang1 <fredfang1203@gmail.com>
2022-04-25 15:12:56 +1000

    Update json_reader..py

--- json_reader..py
+++ json_reader..py
@@ -2,18 +2,20 @@ import json
 import random
 
 def load_tweets():
+    print('default reading gg2013.json')
     with open('data/gg2013.json') as tweets_file:
         tweets = json.load(tweets_file)
     return tweets
 
 def load_tweets(filename):
     path = 'data/' + filename
+    print('Reading {}'.format(filename))
     with open(path) as tweets_file:
         tweets = json.load(tweets_file)
+    print('Finish reading {}'.format(filename))
     return tweets
 
 if __name__ == '__main__':
-    print('load gg2015.json')
     tweets = load_tweets('gg2015.json')
     # demo print
     print(type(tweets))
@@ -21,8 +23,8 @@ if __name__ == '__main__':
     print(type(tweets[0]))
     print('text' in tweets[0])
     print(tweets[0])
+    print()
 
-    print('\nload gg2013.json')
     tweets = load_tweets('gg2013.json')
     # demo print
     print(tweets[0])

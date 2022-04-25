3211c7db1b1a6e2a39e81ae43773ddfc38781acf
lfang1 <fredfang1203@gmail.com>
2022-04-25 15:07:27 +1000

    Update json_reader..py
    load_tweets with filename parameter

--- json_reader..py
+++ json_reader..py
@@ -6,11 +6,27 @@ def load_tweets():
         tweets = json.load(tweets_file)
     return tweets
 
+def load_tweets(filename):
+    path = 'data/' + filename
+    with open(path) as tweets_file:
+        tweets = json.load(tweets_file)
+    return tweets
 
 if __name__ == '__main__':
-    tweets = load_tweets()
+    print('load gg2015.json')
+    tweets = load_tweets('gg2015.json')
     # demo print
     print(type(tweets))
     print('text' in tweets)
-    for i in range(10):
-        print(tweets[i])
+    print(type(tweets[0]))
+    print('text' in tweets[0])
+    print(tweets[0])
+
+    print('load gg2013.json')
+    tweets = load_tweets('gg2013.json')
+    # demo print
+    print(type(tweets))
+    print('text' in tweets)
+    print(type(tweets[0]))
+    print('text' in tweets[0])
+    print(tweets[0])

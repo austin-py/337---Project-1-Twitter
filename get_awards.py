39d5e64b9946f292997ec025fbe04f22bfea72b7
austin-py <austinporras2023@u.northwestern.edu>
2022-04-30 11:16:43 -0500

    Cleaned up file

--- get_awards.py
+++ get_awards.py
@@ -3,6 +3,36 @@ import re
 from json_reader import load_tweets
 from award_names import award_names_w_best as anwb
 
+
+def clean_up(potential_awards):
+    """
+    Input:
+        - Takes a list of list of awards which contain the name, and the number of times it was mentioned
+        - [[Best Drama, 45], [Best Picture, 57]] etc 
+    Output:
+        - Returns the list with updated counts. This tries to take longer sentences such as 
+          "best drama at the golden globes goes to _____" and still use them in the ocunt for "best drama"
+    """
+    for n in potential_awards:
+        for name in potential_awards:
+            if n[0] in name[0]:
+                n[1] += 1
+            elif name[0] in n[0]:
+                name[1] +=1 
+    return potential_awards
+
+def print_awards(awards):
+    """
+    Input:
+        - Takes a list of list of awards which contain the name, and the number of times it was mentioned
+        - [[Best Drama, 45], [Best Picture, 57]] etc 
+
+    Output:
+        - Nothing is returned, but the awards are pritned to stdout. 
+    """
+    for name in awards:
+        print(name[0], "............", name[1])
+
 def get_awards(filename=None):
     """
     Input: 
@@ -28,52 +58,33 @@ def get_awards(filename=None):
         UPPER_BOUND = 0.00028629833431629095 #Found by 50/#2013 tweets 
         if potential_names[potential_name] > (LOWER_BOUND * num_tweets) and potential_names[potential_name] <= (UPPER_BOUND * num_tweets) :
             high_potential_names.append([potential_name,potential_names[potential_name]])
-            #print(potential_name, " was repeated ", potential_names[potential_name], " times\n")
-        if potential_names[potential_name] > (UPPER_BOUND * num_tweets):
+        elif potential_names[potential_name] > (UPPER_BOUND * num_tweets):
             people_really_liked.append([potential_name,potential_names[potential_name]])
-
+    
+    high_potential_names = clean_up(high_potential_names)
     high_potential_names.sort(key= lambda x: x[1])
-    for pn in high_potential_names:
-        print(pn[0], " was repeated ", pn[1], " times\n")
 
-    print("\n\n\n")
+
+    people_really_liked = clean_up(people_really_liked)
     people_really_liked.sort(key= lambda x: x[1])
-    for pn in people_really_liked:
-        print(pn[0], " was repeated ", pn[1], " times\n")
 
-    print("There are ", len(high_potential_names), "high potential names.\n")
-    print("There are ", len(people_really_liked), "things that people really liked.\n")
-    print("Processed ", num_tweets, " tweets \n"  )
+    # print_awards(high_potential_names)
+    # print_awards(people_really_liked)
+
+    # print("There are ", len(high_potential_names), "high potential names.\n")
+    # print("There are ", len(people_really_liked), "things that people really liked.\n")
+    # print("Processed ", num_tweets, " tweets \n"  )
     return high_potential_names, people_really_liked
+
+
+
 
 high_potential_names, people_really_liked = get_awards()
 
-
-for n in high_potential_names:
-    for name in high_potential_names:
-        if n[0] in name[0]:
-            n[1] += 1
-        elif name[0] in n[0]:
-            name[1] +=1 
-
-high_potential_names.sort(key = lambda x: x[1])
-for name in high_potential_names:
-    print(name[0], "............", name[1])
+print_awards(high_potential_names)
+print_awards(people_really_liked)
 
 
-# high_potential_hits = 0
-# people_really_liked_hits = 0 
-# for award in anwb:
-    # for name in high_potential_names:
-        # if award.lower() in name[0] or award.lower().strip() == name[0]:
-            # print(name, award.lower())
-            # high_potential_hits += 1 
-    # for name in people_really_liked:
-        # if award.lower() in name[0]:
-            # people_really_liked_hits += 1
-# 
-# print("\n\n\nThere were ", high_potential_hits, " high-potential hits\n")
-# print("There were ", people_really_liked_hits, " people really liked hits\n")
 """
 -Best dressed or other sentiment votes as voted by twitter might be fairly easy to generate while doing this as well. 
 -We might want to tag tweets that we get award names from, because they likely also have information about the nominees or winners. 

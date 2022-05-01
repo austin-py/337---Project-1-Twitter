d4f0d310b2faebb49390d4d71137c2de99e2dc10
lfang1 <fredfang1203@gmail.com>
2022-05-01 10:59:01 +1000

    Implement save_tweets to save text as txt

--- json_reader.py
+++ json_reader.py
@@ -11,15 +11,25 @@ def load_tweets(filename=None):
     print('Finish reading {}'.format(filename))
     return tweets
 
+def save_tweets(output_name):
+    path = 'data/' + output_name
+    f = open(path, 'w')
+    tweets = load_tweets('gg2013.json')
+    for t in tweets:
+        f.write(t['text'] + '\n')
+    f.close()
+
+
 if __name__ == '__main__':
-    tweets = load_tweets('gg2015.json')
+    save_tweets('gg2013_text.txt')
+    #tweets = load_tweets('gg2015.json')
     # demo print
-    print(type(tweets))
-    print('text' in tweets)
-    print(type(tweets[0]))
-    print('text' in tweets[0])
-    print(tweets[0])
-    print('Total tweets: {}'.format(len(tweets)))
-    unique_user_ids = list()
-    print(tweets[0]['user'])
-    print(tweets[0]['user']['id'])
+    # print(type(tweets))
+    # print('text' in tweets)
+    # print(type(tweets[0]))
+    # print('text' in tweets[0])
+    # print(tweets[0])
+    # print('Total tweets: {}'.format(len(tweets)))
+    # unique_user_ids = list()
+    # print(tweets[0]['user'])
+    # print(tweets[0]['user']['id'])

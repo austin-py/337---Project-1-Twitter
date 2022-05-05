1d345c61801df229fa5e7b06f267f90184fb27bc
austin-py <austinporras2023@u.northwestern.edu>
2022-05-05 09:50:36 -0700

    Added red-carpet basics

--- get_awards.py
+++ get_awards.py
@@ -12,13 +12,21 @@ def clean_up(potential_awards):
     Output:
         - Returns the list with updated counts. This tries to take longer sentences such as 
           "best drama at the golden globes goes to _____" and still use them in the ocunt for "best drama"
+
+          Current bug: 
+          Double counts since it runs both lists, since proportional for all of them not sure it matters to much. 
+
+          Elimites from things like best comedy series to best comedy... which gets the spirit of the award but doesnt finish it.
+          Basically always shortens, which isn't always what the award names want.  Another example is "Best actor in motion picture -> best actor" 
     """
     for n in potential_awards:
         for name in potential_awards:
             if n[0] in name[0]:
+                print("ADDED COUNT TO", n[0], "PROGRAM THINKS ", n[0], " IN ", name[0])
                 n[1] += 1
             elif name[0] in n[0]:
                 name[1] +=1 
+                print("ADDED COUNT TO", name[0], "PROGRAM THINKS ", name[0], " IN ", n[0])
     return potential_awards
 
 def print_awards(awards):
@@ -60,29 +68,68 @@ def get_awards(filename=None):
             high_potential_names.append([potential_name,potential_names[potential_name]])
         elif potential_names[potential_name] > (UPPER_BOUND * num_tweets):
             people_really_liked.append([potential_name,potential_names[potential_name]])
-    
+
     high_potential_names = clean_up(high_potential_names)
     high_potential_names.sort(key= lambda x: x[1])
 
 
     people_really_liked = clean_up(people_really_liked)
     people_really_liked.sort(key= lambda x: x[1])
-
-    # print_awards(high_potential_names)
-    # print_awards(people_really_liked)
-
-    # print("There are ", len(high_potential_names), "high potential names.\n")
-    # print("There are ", len(people_really_liked), "things that people really liked.\n")
-    # print("Processed ", num_tweets, " tweets \n"  )
     return high_potential_names, people_really_liked
 
+def get_best_dressed(filename=None):
+    """
+    Input: 
+        - Optionally takes a filename from the data folder, defaults to Golden Globes 2013 if not given one. 
+    Output:
+        - Outputs the twitter voted best-dressed. 
+    """
+    tweets = load_tweets(filename=filename)
+    num_tweets = 0
+    potential_names = {}
+    just_names = {}
+    for tweet in tweets:
+        num_tweets += 1 
+        if 'best' in tweet['text']: # It is significantly faster to check this here then just let regex do it all. 
+            test = re.findall("best dressed [a-zA-Z\s]+",tweet['text'],flags=re.IGNORECASE)
+            test2 = re.findall("[a-zA-Z\s]+ best dressed",tweet['text'],flags=re.IGNORECASE)
+            for item in test:
+                temp = re.findall('([A-Z][a-z]+(?=\s[A-Z])(?:\s[A-Z][a-z]+)+)',item)
+                for i in temp:
+                    just_names[i] = just_names.get(i,0) + 1
+                potential_names[item] = potential_names.get(item,0) + 1
+            for item in test2:
+                temp = re.findall('([A-Z][a-z]+(?=\s[A-Z])(?:\s[A-Z][a-z]+)+)',item)
+                for i in temp:
+                    just_names[i] = just_names.get(i,0) + 1
+                potential_names[item] = potential_names.get(item,0) + 1
+    
+    extra_temp = []
+    for i in just_names.keys():
+        extra_temp.append([i,just_names[i]])
+
+    extra_temp.sort(key= lambda x: x[1])
+    extra_temp.reverse()
+
+    return extra_temp
 
 
 
-high_potential_names, people_really_liked = get_awards()
 
-print_awards(high_potential_names)
-print_awards(people_really_liked)
+#high_potential_names, people_really_liked = get_awards()
+temp = get_best_dressed('gg2015.json')
+for i in range(10):
+    print(temp[i])
+print(temp[0][0])
+
+
+
+
+
+
+
+#print_awards(high_potential_names)
+#print_awards(people_really_liked)
 
 
 """

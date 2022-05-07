06f042b062a6bb0f8a8ddc125690ebcc513dc40b
lfang1 <fredfang1203@gmail.com>
2022-05-07 22:53:11 +1000

    Update host_parser.py
    Get accurate result

--- host_parser.py
+++ host_parser.py
@@ -1,16 +1,16 @@
 from collections import OrderedDict
-
+import nltk
 from json_reader import *
 import re
 
 def clean_tweet(input_tweet):
     amp_replaced = replace_amp(input_tweet)
     nonword_removed = remove_nonword_char(amp_replaced)
-    cleaned = nonword_removed.lower()
-    return cleaned
+    #cleaned = nonword_removed.lower()
+    return nonword_removed
 
 def remove_nonword_char(input_tweet):
-    nonword_removed = re.sub(r'[\W]^\s', '', input_tweet)
+    nonword_removed = re.sub(r'[^\w\s]', '', input_tweet)
     return nonword_removed
 
 def replace_amp(input_tweet):
@@ -75,7 +75,7 @@ def get_host_majority_vote(tweets):
         present_result, present_participle_result, plural_noun_result, past_tense_result)
     return majority_vote_result
 
-# get top 10 answers
+# get top n answers
 def rank_and_select_final_answers(*args):
     final_result = {}
     top_n = 10
@@ -89,8 +89,33 @@ def rank_and_select_final_answers(*args):
                 final_result[k] = sorted_result[k]
 
     sorted_final_result = dict(sorted(final_result.items(), key=lambda x:x[1], reverse=True))
-    final_top_n_answers = list(sorted_final_result.keys())[:top_n]
+    final_top_n_answers = list(sorted_final_result.keys())[:2]
+    # ans_prob = {}
+    # for i in range(len(final_top_n_answers)):
+    #     ans = final_top_n_answers[i]
+    #     length = len(ans.split())
+    #     ans_prob[ans] = 0.1
+    #     if length > 2:
+    #         for prev_a in final_top_n_answers[:i]:
+    #             if prev_a in ans:
+    #                 ans_prob[prev_a] *= 10
+    #
+    #
+    # sorted_refined_final = dict(sorted(ans_prob.items(), key=lambda x:x[1], reverse=True))
+    # sorted_top_2 = list(sorted_refined_final.keys())[:10]
+
     return final_top_n_answers
+
+###current performance by top_n = 10
+    # gg2013_clean.json:
+    # ['Tina Fey', 'Amy Poehler', 'Will Ferrell', 'Kristen Wiig', 'Tina Fey Amy', 'Tina Fey Amy Poelhers',
+    #  'Tina Fey Amy Poelhers Opening', 'Tina Fey Amy Poelhers Opening Monologue', 'should Will Ferrell',
+    #  'Poehler should']
+
+    # gg2015_clean.json:
+    # ['Tina Fey', 'Amy Poehler', 'Kristen Wiig', 'Tackle Tough', 'Tackle Tough Topics', 'Tackle Tough Topics With',
+    #  'Tackle Tough Topics With Comedy', 'theyre also Sisters', 'Tackle Tough Topics With Comedy ABC',
+    #  'theyre also Sisters in']
 
 
 
@@ -100,29 +125,43 @@ def print_host_by_form(input_tweet, form):
     if form in words:
         print(input_tweet)
 
-def get_host_rule_present(input_tweet, form):
+def get_host_rule_present(cleaned_tweet, form):
     if form.lower() != 'host':
         print("Wrong rule \'host\' implemented for form: {}".format(form))
         return None
-    cleaned_tweet = clean_tweet(input_tweet)
-    end = cleaned_tweet.index('host')
+
+    end = cleaned_tweet.lower().index('host')
     subtweet = cleaned_tweet[:end]
-    words = subtweet.lower().split()
+    words = subtweet.split()
     candidate_answers = []
     # from index of 'host' to the beginning
     for i in range(len(words)-1, -1, -1):
         candidate_answer = ' '.join(words[i:len(words)])
-        candidate_answers.append(candidate_answer)
+        if any(char.isupper() for char in candidate_answer):
+            length = len(candidate_answer.split())
+            if length > 1:
+                if 'and' in candidate_answer.lower():
+                    answers = candidate_answer.split('and')
+                    for ans in answers:
+                        if len(ans) == 0 or len(ans.split()) < 2:
+                            continue
+                        ans_stripped = ans.strip()
+                        if not ans_stripped in candidate_answers:
+                            candidate_answers.append(ans_stripped)
+                else:
+                    candidate_answers.append(candidate_answer)
+            if length > 5:
+                return candidate_answers
+
     return candidate_answers
 
-def get_host_rule_present_participle(input_tweet, form):
+def get_host_rule_present_participle(cleaned_tweet, form):
     if form.lower() != 'hosting':
         print("Wrong rule \'hosting\' implemented for form: {}".format(form))
         return None
-    cleaned_tweet = clean_tweet(input_tweet)
-    end = cleaned_tweet.index('hosting')
+    end = cleaned_tweet.lower().index('hosting')
     subtweet = cleaned_tweet[:end]
-    words = subtweet.lower().split()
+    words = subtweet.split()
     if len(words) == 0:
         return None
     try:
@@ -139,56 +178,109 @@ def get_host_rule_present_participle(input_tweet, form):
     # from index of 'are' to the beginning
     for i in range(len(words)-2, -1, -1):
         candidate_answer = ' '.join(words[i:len(words)-1])
-        candidate_answers.append(candidate_answer)
+        if any(char.isupper() for char in candidate_answer):
+            length = len(candidate_answer.split())
+            if length > 1:
+                if 'and' in candidate_answer.lower():
+                    answers = candidate_answer.split('and')
+                    for ans in answers:
+                        if len(ans) == 0 or len(ans.split()) < 2:
+                            continue
+                        ans_stripped = ans.strip()
+                        if not ans_stripped in candidate_answers:
+                            candidate_answers.append(ans_stripped)
+                else:
+                    candidate_answers.append(candidate_answer)
+            if length > 5:
+                return candidate_answers
     return candidate_answers
 
 
-def get_host_rule_plural_noun(input_tweet, form):
+def get_host_rule_plural_noun(cleaned_tweet, form):
     if form.lower() != 'hosts':
         print("Wrong rule \'hosts\' implemented for form: {}".format(form))
         return None
-    cleaned_tweet = clean_tweet(input_tweet)
-    start = cleaned_tweet.index('hosts') + len('hosts')
+    start = cleaned_tweet.lower().index('hosts') + len('hosts')
     subtweet = cleaned_tweet[start:]
-    words = subtweet.lower().split()
+    words = subtweet.split()
     candidate_answers = []
     # from index of 'hosts' to the beginning
     for i in range(len(words)):
         candidate_answer = ' '.join(words[0:i+1])
-        candidate_answers.append(candidate_answer)
+        if any(char.isupper() for char in candidate_answer):
+            length = len(candidate_answer.split())
+            if length > 1:
+                if 'and' in candidate_answer.lower():
+                    answers = candidate_answer.split('and')
+                    for ans in answers:
+                        if len(ans) == 0 or len(ans.split()) < 2:
+                            continue
+                        ans_stripped = ans.strip()
+                        if not ans_stripped in candidate_answers:
+                            candidate_answers.append(ans_stripped)
+                else:
+                    candidate_answers.append(candidate_answer)
+            if length > 5:
+                return candidate_answers
     return candidate_answers
 
-def get_host_rule_past_tense(input_tweet, form):
+def get_host_rule_past_tense(cleaned_tweet, form):
     if form.lower() != 'hosted':
         print("Wrong rule \'hosted\' implemented for form: {}".format(form))
         return None
-    if 'hosted by' in input_tweet:
-        return get_host_rule_passive(input_tweet, 'hosted by')
+    if 'hosted by' in cleaned_tweet:
+        return get_host_rule_passive(cleaned_tweet, 'hosted by')
 
-    cleaned_tweet = clean_tweet(input_tweet)
-    end = cleaned_tweet.index('hosted')
+    end = cleaned_tweet.lower().index('hosted')
     subtweet = cleaned_tweet[:end]
-    words = subtweet.lower().split()
+    words = subtweet.split()
     candidate_answers = []
     # from index of 'hosts' to the beginning
     for i in range(len(words)-1, -1, -1):
         candidate_answer = ' '.join(words[i:end])
-        candidate_answers.append(candidate_answer)
+        if any(char.isupper() for char in candidate_answer):
+            length = len(candidate_answer.split())
+            if length > 1:
+                if 'and' in candidate_answer.lower():
+                    answers = candidate_answer.split('and')
+                    for ans in answers:
+                        if len(ans) == 0 or len(ans.split()) < 2:
+                            continue
+                        ans_stripped = ans.strip()
+                        if not ans_stripped in candidate_answers:
+                            candidate_answers.append(ans_stripped)
+                else:
+                    candidate_answers.append(candidate_answer)
+            if length > 5:
+                return candidate_answers
     return candidate_answers
 
-def get_host_rule_passive(input_tweet, form):
-    if not 'hosted by' in input_tweet:
+def get_host_rule_passive(cleaned_tweet, form):
+    if not 'hosted by' in cleaned_tweet:
         print("Wrong rule \'hosted by\' implemented for form: {}".format(form))
 
-    cleaned_tweet = clean_tweet(input_tweet)
-    start = cleaned_tweet.index('hosted by') + len('hosted by')
+    start = cleaned_tweet.lower().index('hosted by') + len('hosted by')
     subtweet = cleaned_tweet[start:]
-    words = subtweet.lower().split()
+    words = subtweet.split()
     candidate_answers = []
     # from index of 'hosts' to the beginning
     for i in range(len(words)):
         candidate_answer = ' '.join(words[0:i+1])
-        candidate_answers.append(candidate_answer)
+        if any(char.isupper() for char in candidate_answer):
+            length = len(candidate_answer.split())
+            if length > 1:
+                if 'and' in candidate_answer.lower():
+                    answers = candidate_answer.split('and')
+                    for ans in answers:
+                        if len(ans) == 0 or len(ans.split()) < 2:
+                            continue
+                        ans_stripped = ans.strip()
+                        if not ans_stripped in candidate_answers:
+                            candidate_answers.append(ans_stripped)
+                else:
+                    candidate_answers.append(candidate_answer)
+            if length > 5:
+                return candidate_answers
     return candidate_answers
 
 
@@ -224,21 +316,21 @@ def find_common_phrase(filename=None):
     return sorted_top_10_words
 
 def main():
-    #print(get_host_rule_present('Joh Doe and Steve Smith host Golden Globe 2022', 'host'))
+    #print(get_host_rule_present('Tina Fey and Amy Poehler host Golden Globe 2022', 'host'))
     #print(get_host_rule_present_participle('Actually excited for the Golden Globes this year because Tine Fey and Amy Poehler are hosting. :D I love them. #SecondCity', 'hosting'))
-    #print(get_host_rule_present_participle('Now #GoldenGlobes, when you said Tina &amp; Amy were hosting, it was more of "Hey we need an opening act" thing right.', 'hosting'))
-    #print(get_host_rule_plural_noun('RT @goldenglobes: It\'s our hosts Tina Fey and Amy Poehler! #goldenglobes \
-        #redcarpet http://t.co/8lqC3ocQ', 'hosts'))
+    #print(get_host_rule_present_participle(clean_tweet('Now #GoldenGlobes, when you said Tina &amp; Amy were hosting, it was more of "Hey we need an opening act" thing right.'), 'hosting'))
+    #print(get_host_rule_plural_noun(clean_tweet('RT @goldenglobes: It\'s our hosts Tina Fey and Amy Poehler! #goldenglobes \
+        #redcarpet http://t.co/8lqC3ocQ'), 'hosts'))
     #print(get_host_rule_past_tense('RT @thetimes: Tonight\'s Golden Globes will be hosted Tina Fey and Amy Poehler. Full nominations here: http://t.co/xEjTpufI http://t.co/NQy5itil', 'hosted'))
     #print(get_host_rule_past_tense('RT @LaurieCrosswell: Every awards show from now on should be hosted by Amy Poelher and Tina Fey. #goldenglobes', 'hosted'))
     # print(remove_nonword_char("Striker@#$__.. as1%23"))
     #print(find_common_phrase())
     #print(find_common_phrase('gg2015.json'))
 
-    tweets = load_tweets('gg2013.json')
+    tweets = load_tweets('gg2013_clean.json')
     print(get_host_majority_vote(tweets))
-
-    tweets = load_tweets('gg2015.json')
+    #
+    tweets = load_tweets('gg2015_clean.json')
     print(get_host_majority_vote(tweets))
 
 

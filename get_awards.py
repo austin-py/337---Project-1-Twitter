130a002204504133c773c8cef62f16adaf394d3d
austin-py <austinporras2023@u.northwestern.edu>
2022-05-17 10:28:28 -0500

    name change

--- get_awards.py
+++ get_awards.py
@@ -43,7 +43,7 @@ def clean_awards(awards):
     
     return awards
 
-def get_awards(tweets):
+def return_awards(tweets):
     """
     Input: 
         - Takes a dictionary of tweets from json reader
@@ -190,7 +190,7 @@ def get_worst_dressed(tweets):
 
 if __name__ == '__main__':
     tweets = load_tweets(FILE)
-    award_names = get_awards(tweets)
+    award_names = return_awards(tweets)
     for i in range(26):
         print("Our ", i, "th pick for award_name is ", award_names[i], ".",sep="")
     temp = get_best_dressed(tweets)
